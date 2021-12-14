b873835eac85b9edb1c220772d90741fa9a3a8ed
olgaokhr <olyunechkavip@gmail.com>
2021-12-14 13:55:32 +0300

    commit

--- def_credit_card.py
+++ def_credit_card.py
@@ -1,6 +1,6 @@
 def credit_card(number_card):
     if number_card.isdigit()==True and len(number_card)==16:
-        return '**** **** **** '+number_card[-4:]
+        return '************'+number_card[-4:]
     else:
         print('Неправильно введен номер карты')
 
